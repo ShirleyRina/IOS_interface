7edda7651d06f73d190ca30230584aa23fb05e8d
ShirleyRina <shuangling7890@gmail.com>
2024-09-19 18:54:24 -0500

    zoom功能修好了！嘻嘻

--- 7323assignment1/ImageZoomViewController.swift
+++ 7323assignment1/ImageZoomViewController.swift
@@ -1,8 +1,9 @@
 import UIKit
 
-class ImageZoomViewController: UIViewController {
+class ImageZoomViewController: UIViewController, UIScrollViewDelegate {
 
     var imageView: UIImageView!
+    var scrollView: UIScrollView!
     var image: UIImage? // 存储要放大的图片
 
     override func viewDidLoad() {
@@ -11,11 +12,21 @@ class ImageZoomViewController: UIViewController {
         // 设置背景为半透明黑色
         view.backgroundColor = UIColor.black.withAlphaComponent(0.8)
 
+        // 创建 UIScrollView 用于支持缩放
+        scrollView = UIScrollView(frame: view.bounds)
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 1.0  // 最小缩放比例
+        scrollView.maximumZoomScale = 3.0  // 最大缩放比例
+        scrollView.contentSize = view.bounds.size
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        view.addSubview(scrollView)
+
         // 创建 UIImageView 用来显示放大的图片
-        imageView = UIImageView(frame: view.bounds)
+        imageView = UIImageView(frame: scrollView.bounds)
         imageView.contentMode = .scaleAspectFit
         imageView.image = image
-        view.addSubview(imageView)
+        scrollView.addSubview(imageView)
 
         // 添加点击手势，点击空白区域关闭视图
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissZoomView))
@@ -26,5 +37,9 @@ class ImageZoomViewController: UIViewController {
     @objc func dismissZoomView() {
         self.dismiss(animated: true, completion: nil)
     }
-}
 
+    // UIScrollViewDelegate 方法，指定要缩放的视图
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+}
