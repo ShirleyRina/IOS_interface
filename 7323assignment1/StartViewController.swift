546d65d041404cfc3427f0448a85b25f0b64e1f6
ShirleyRina <shuangling7890@gmail.com>
2024-09-19 19:43:52 -0500

    stepper调整字号大小， lets go button跳转页面

---
+++ 7323assignment1/StartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StartViewController.swift
+//  7323assignment1
+//
+//  Created by shirley on 9/19/24.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
